f78574550c9ee90077445449cf00aa37d4876b2b
Fernando Carlos <111884680+Solarck@users.noreply.github.com>
2022-11-11 18:28:58 -0300

    up

---
+++ script.js
@@ -0,0 +1,15 @@
+const password = document.getElementById('password');
+const icon = document.getElementById('icon');
+
+function EyeClick() {
+
+    if (password.type === 'password') {
+        password.setAttribute('type', 'text');
+        icon.setAttribute('src', 'imgs/open-eye.svg')
+    }
+
+    else {
+        password.setAttribute('type', 'password');
+        icon.setAttribute('src', 'imgs/closed-eye.svg')
+    }
+}
